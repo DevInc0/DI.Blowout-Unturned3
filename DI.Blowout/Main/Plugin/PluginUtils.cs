d268d1d2ff7dac33f0891103fce0fd2a70d63060
DevInc <defaultdev0x101@gmail.com>
2022-04-19 18:02:24 +0300

    Code refactoring

--- DI.Blowout/Main/Plugin/PluginUtils.cs
+++ DI.Blowout/Main/Plugin/PluginUtils.cs
@@ -9,21 +9,29 @@ namespace DI.Blowout.Main
         private void SendUIEffectToAllPlayers(ushort id)
         {
             foreach (SteamPlayer steamPlayer in Provider.clients)
+            {
                 EffectManager.sendUIEffect(id, 15001, steamPlayer.transportConnection, true);
+            }
         }
 
         private void ClearUIEffectFromAllPlayers(ushort id)
         {
             foreach (SteamPlayer steamPlayer in Provider.clients)
+            {
                 EffectManager.askEffectClearByID(id, steamPlayer.transportConnection);
+            }
         }
 
         private void DamagePlayers()
         {
             foreach (SteamPlayer steamPlayer in Provider.clients)
             {
-                if (!Physics.Raycast(new Ray(steamPlayer.player.look.aim.position, Vector3.up), float.MaxValue, RayMasks.ROOFS_INTERACT)) // Если нет крыши над головой, бей
-                    steamPlayer.player.life.askDamage(Configuration.DamagePerTick, Vector3.right, EDeathCause.ACID, ELimb.SKULL, CSteamID.Nil, out _);
+                Player player = steamPlayer.player;
+                
+                if (Physics.Raycast(new Ray(player.look.aim.position, Vector3.up), float.MaxValue, RayMasks.ROOFS_INTERACT) == false)
+                {
+                    player.life.askDamage(Configuration.DamagePerTick, Vector3.right, EDeathCause.ACID, ELimb.SKULL, CSteamID.Nil, out _);
+                }
             }
         }
     }
